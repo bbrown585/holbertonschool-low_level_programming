d6f2eb6714891930b89c257bf671b4db4fb5ccf2
bbrown585 <brandon_scott585@yahoo.com>
2022-05-20 13:02:41 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main(void)
 {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long));
