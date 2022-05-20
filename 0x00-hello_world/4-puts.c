f1f9a084356f8ae358ec7b36f5c5dea88acc233f
bbrown585 <brandon_scott585@yahoo.com>
2022-05-20 11:49:34 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char str1[] 
-	
-	strcpy(str1, "Programming is like building a multilingual puzzle"
-			
-	puts(str1);
-	
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 	
 
