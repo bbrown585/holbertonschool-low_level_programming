22cf0a5895d12b3f8e9693affc94419a672c5dde
bbrown585 <brandon_scott585@yahoo.com>
2022-05-23 15:31:34 -0700

    More c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - 1 to 9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = '1';
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
