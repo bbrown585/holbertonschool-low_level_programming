ac2128c08045174537c92b4069996683bcbc2182
bbrown585 <brandon_scott585@yahoo.com>
2022-05-23 19:12:56 -0700

    More c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
-
+/**
+*main - Alpha a to z
+*
+* Return: Always 0 ( sucessful)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'q' && ch != 'e')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
