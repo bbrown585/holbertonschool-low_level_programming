df8302f5f89dd13ad760b93922030a65b6bb22e2
bbrown585 <brandon_scott585@yahoo.com>
2022-05-23 19:41:07 -0700

    More c

--- 0x01-variables_if_else_while/9-print_combo.c
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
+/**
+* main - printing
+*
+* Return: Always 0 (sucessful)
+*/
+int main(void)
+{
+int i;
 
+for (i = 48 ; i < 58 ; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
+}
