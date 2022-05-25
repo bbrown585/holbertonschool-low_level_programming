1f1607b8462a7c28f12a351bb16fb58456b02e50
bbrown585 <brandon_scott585@yahoo.com>
2022-05-25 14:28:17 -0700

    Functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,27 @@
-#include
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - idk
+ * @n: - idk
+ * Return: 0.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+		while (n < 98)
+		{
+		printf("%d", n);
+			n--;
+		}
+	else if (n > 98)
+		while (n < 98)
+		{
+		printf("%d", n);
+			n++;
+		}
+	else
+	{
+		printf("98\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void times_table(void);
 void jack_bauer(void);
 int add(int, int);
 int print_last_digit(int);
+void print_to_98(int n);
