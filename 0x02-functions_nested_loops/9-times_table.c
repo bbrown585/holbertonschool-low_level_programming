8774800a1272ed22a471345c891ec743e5d41af5
bbrown585 <brandon_scott585@yahoo.com>
2022-05-25 13:13:00 -0700

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,33 @@
-#include
+#include "main.h"
+
+/**
+ *times_table - idk
+ *
+ */
+void times_table(void)
+{
+	int num;
+	int mult;
+	int prod;
+
+		for (num = 0 ; num <= 9 ; num++)
+		{
+			_putchar('0');
+			for (mult = 1 ; mult <= 9 ; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('0' + (prod / 10));
+				}
+				_putchar('0' + (prod % 10));
+			}
+			_putchar('\n');
+		}
+}
