bdf55f445b9d5bbb7d01deee3604163197915a40
bbrown585 <brandon_scott585@yahoo.com>
2022-05-25 13:49:48 -0700

    Functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,16 @@
-#include
+#include "main.h"
+
+/**
+ * print_last_digit - idk
+ * @b: -idk
+ * Return: Always 0.
+ */
+
+int print_last_digit(int b)
+{
+	b = (b % 10);
+	if (b < 0)
+	b = b * -1;
+	_putchar(b + 48);
+	return (b);
+}
