34cf6b50a99a260c6ce35ae4c445f37a67710390
bbrown585 <brandon_scott585@yahoo.com>
2022-05-24 18:41:02 -0700

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphatbet - none
+ *
+ * Return: none
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i++ < 10)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar(c);
+
+		_putchar(10);
+	}
+}
