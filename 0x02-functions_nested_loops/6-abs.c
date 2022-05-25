c45f018eb19c5d8442c62de26077c9f06f5c784b
bbrown585 <brandon_scott585@yahoo.com>
2022-05-25 09:56:45 -0700

    Functions

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,18 @@
-#include
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_abs - check the code
+ *@n:
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+
+	if (n > 48)
+{
+	n = n * (-1);
+}
+return (n);
+}
