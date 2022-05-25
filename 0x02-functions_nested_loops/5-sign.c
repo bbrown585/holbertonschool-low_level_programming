95471f3914f4523ba1d5103a1bb9a028ff9437fe
bbrown585 <brandon_scott585@yahoo.com>
2022-05-25 09:25:26 -0700

    Functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ *print_sign - check the code.
+ *@n: idk
  *
  * Return: Always 0.
  */
-int main(void)
+int print_sign(int n)
 {
+if (n >= 48)
+{
+_putchar(43);
+		return (1);
+}
+	else if (n == 48)
+{
+		_putchar(48);
+	return (0);
+}
+	else
+{
+		_putchar(45);
+	return (-1);
+}
+}
