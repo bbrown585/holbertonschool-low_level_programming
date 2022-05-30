714b64ab4faf15fbab0a747f2b4f7a38b8850e27
bbrown585 <brandon_scott585@yahoo.com>
2022-05-30 15:43:24 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - one to nine
+ * ch: idk
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	char ch = '1';
+
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+}
