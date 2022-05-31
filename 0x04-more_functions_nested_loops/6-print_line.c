9c6c5924f7e73af53a7d0ac8585d4f703a799a02
bbrown585 <brandon_scott585@yahoo.com>
2022-05-31 12:27:53 -0700

    More functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,11 +3,17 @@
 
 /**
  * print_line - print
- *
- *
+ * @n: idk
  * Return: 0
  */
-void print_line(int)
+void print_line(int n)
 {
 	int i;
 
+	if (n > 0)
+	{
+		for (i = 0; i < 10; i++)
+		putchar('_');
+	}
+		putchar('\n');
+}
