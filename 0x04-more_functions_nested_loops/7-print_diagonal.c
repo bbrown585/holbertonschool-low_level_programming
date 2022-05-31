03fb7157bb8111ee1623e8974a698a06c0f4f0a1
bbrown585 <brandon_scott585@yahoo.com>
2022-05-31 16:34:20 -0700

    More functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,11 +2,29 @@
 #include <stdio.h>
 
 /**
+ * print_diagonal - hmm
+ * @n: print
  *
- *
- *
- * Return:
+ * Return: void
  */
 void print_diagonal(int n)
 {
+	int b, k;
 
+	if (n > 0)
+	{
+		for (b = 0; b < n; b++)
+		{
+			for (k = 0; k < b; k++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
