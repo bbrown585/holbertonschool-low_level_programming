c031e469e7eeae26069f16d33af217371c2ca657
bbrown585 <brandon_scott585@yahoo.com>
2022-06-01 12:02:30 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - string length
+ * @s: idk
+ * Return:0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while
+(s[length] != '\0') length++);
+	return (length);
+}
