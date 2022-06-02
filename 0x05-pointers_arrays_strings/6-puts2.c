0361bdb9f9ba2a22cdb782f4cc6f82aacc0467cc
bbrown585 <brandon_scott585@yahoo.com>
2022-06-02 12:46:50 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - idk
+ * @str: idk
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+	}
+	for (i = 0; i <= j - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	{
+		_putchar('\n');
+	}
+}
