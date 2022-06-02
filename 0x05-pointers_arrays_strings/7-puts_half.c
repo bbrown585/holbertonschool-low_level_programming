d163856abd8b988df801170d35ed642adf24c3a1
bbrown585 <brandon_scott585@yahoo.com>
2022-06-02 13:49:56 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - idk
+ * @str: idk
+ * Return: 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while
+		(str[i] != '\n' && str[i] != '\0')
+			i++;
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+			_putchar(str[j]);
+	}
+
+	else
+	{
+		for (j = i / 2 + 1; j < i; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
