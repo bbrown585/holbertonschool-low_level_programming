6c46cc7965c409b9c71c661528516826ae03ebe2
bbrown585 <brandon_scott585@yahoo.com>
2022-06-02 19:27:56 -0700

    Pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,19 +10,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
-	{
 		for (i = 0; i < n; i++)
 		{
-			if (i != n - 1)
-			{
+			if (i == 0)
 				printf("%d", a[i]);
-			}
 			else
-			{
 				printf("%d", a[i]);
-			}
-			printf("\n");
 		}
-	}
+		printf("\n");
 }
