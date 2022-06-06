78a19d60d65ecb59b91a433577b8f621901f4cfe
bbrown585 <brandon_scott585@yahoo.com>
2022-06-06 14:27:06 -0700

    more of this

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - concat
+ * @src: pointers
+ * @dest: pointers
+ *
+ * Return: 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *begin = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (begin);
+}
