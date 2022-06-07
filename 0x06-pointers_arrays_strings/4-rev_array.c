ab8aec9fc913c706e710daaba3486e191950afa7
bbrown585 <brandon_scott585@yahoo.com>
2022-06-06 17:53:09 -0700

    more of this

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse reverse
+ * @a: array
+ * @n: idk
+ *
+ * Return: 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int bb;
+	int temp;
+	int *z = a;
+
+	for (bb = 0; bb < (n - 1); bb++)
+		z++;
+	for (bb = 0; bb <= n; bb++)
+		while (a < z)
+		{
+			temp = *a;
+			*a = *z;
+			*z = temp;
+			a++;
+			z++;
+		}
+}
