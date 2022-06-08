9c62a3bff2f566cf52c510227d76ed66308ec9bc
bbrown585 <brandon_scott585@yahoo.com>
2022-06-07 19:05:13 -0700

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of two square matrix
+ * @a: array
+ * @size: not sure
+ *
+ * Return: 0.
+ */
+void print_diagsums(int *a, int size)
+{
+
+
+
+
+
+
+
+
+
+
+}
