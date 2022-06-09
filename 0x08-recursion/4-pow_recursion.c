cff1c0e7afa596e8345cd42f8e47a74524ef5155
bbrown585 <brandon_scott585@yahoo.com>
2022-06-08 18:19:28 -0700

    recursions

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ * _pow_recursion - recursion
+ * @x: idk
+ * @y: idk
+ * Return: 0.
  */
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * (_pow_recursion(x, y - 1)));
+}
