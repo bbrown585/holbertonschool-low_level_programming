9751a28a2ff0dfe92917d46d6e1658672e1d8274
bbrown585 <brandon_scott585@yahoo.com>
2022-06-08 20:46:51 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+			return (0);
