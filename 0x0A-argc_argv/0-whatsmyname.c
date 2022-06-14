66c738c7716fea9cc7a310453297d106d2b33b63
bbrown585 <brandon_scott585@yahoo.com>
2022-06-14 05:20:21 -0700

    practice

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argv: name
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{  
+	printf("%\n", argv[0]);
+	return (0);
+}
