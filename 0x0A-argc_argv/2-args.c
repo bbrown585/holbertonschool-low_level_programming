86818aee1dd513fa71a344c92c1230acc51d3845
bbrown585 <brandon_scott585@yahoo.com>
2022-06-14 11:36:44 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-int counter;
-for (counter = 0; counter < argc; counter++)
-printf("argv[%2d]: %s\n", counter, argv[counter]);
+int i = 0;
+for (; i < argc; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
