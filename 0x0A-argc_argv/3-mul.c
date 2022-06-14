4d97422009608805f6abe664b233588beaf4c1db
bbrown585 <brandon_scott585@yahoo.com>
2022-06-14 12:05:22 -0700

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: num
+ * @argv: name
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+
