dcbc09898d524e9f45c95e53a751da7faf5e1748
bbrown585 <brandon_scott585@yahoo.com>
2022-06-14 13:03:42 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,4 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int multiply_num = 0;
 
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		multiply_num = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multiply_num);
+		return (0);
+	}
+}
