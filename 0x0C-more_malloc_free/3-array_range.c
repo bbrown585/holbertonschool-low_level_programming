c463d509339fc627167c5b91709078ae8d05a5e5
bbrown585 <brandon_scott585@yahoo.com>
2022-06-21 12:55:26 -0700

    More malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,10 +4,30 @@
 
 /**
  * array_range - create an array of intergers
- *
- *
- *
+ * @min: minimum
+ * @max: maximum
+ * Return: pointer
  */
 int *array_range(int min, int max)
 {
+	int *arr;
+	int b;
+	int k = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(int) * ((max - min) + 1));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (b = min; b <= max; b++)
+	{
+		arr[k] = b;
+		k++;
+	}
+	return (arr);
+}
 
