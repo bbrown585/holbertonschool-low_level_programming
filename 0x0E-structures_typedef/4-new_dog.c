1bc4b107e4c4a49292531918fa7dd9136f7b9548
bbrown585 <brandon_scott585@yahoo.com>
2022-06-22 18:40:37 -0700

    struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
