37e364bbe54babbaa1f2b15425a2e16db5bfdf2d
Brandon <brandon_scott585@yahoo.com>
2022-07-08 09:22:25 -0700

    updated task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,27 +4,28 @@
 #include <string.h>
 
 /**
- * add_note - add new node
- * @head: head 
+ * add_node - add new node
+ * @head: head
  * @str: string
  * Return: address of element
  */
 list_t *add_node(list_t **head, const char *str)
-{	
+{
 	unsigned int k;
 	list_t *node;
-	
+
 	node = malloc(sizeof(list_t));
 
 	if (node == NULL)
 	return (NULL);
 
-	for (k = 0; str[k] != '\0'  ; k++);
-	
+	for (k = 0; str[k] != '\0'  ; k++)
+	;
+
 	node->len = k;
 	node->str = strdup(str);
 	node->next = *head;
 
 	(*head) = node;
 	return (node);
-}	
+}
