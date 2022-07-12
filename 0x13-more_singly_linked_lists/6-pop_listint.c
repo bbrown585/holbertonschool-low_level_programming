1835de57a1effa62d964038887d50ebaaec36636
Brandon <brandon_scott585@yahoo.com>
2022-07-12 12:30:52 -0700

    updates

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head of a linked list
+ * @head: head of list
+ * Return:0 if empty
+ */
+int pop_listint(listint_t **head)
+{
