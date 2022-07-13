16e83781ddb61d97053e5237cc3bc53b813c5b64
Brandon <brandon_scott585@yahoo.com>
2022-07-12 20:00:23 -0700

    updated main files

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of linked list
+ * @head: head
+ *
+ *
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert new node at given position
+ * @head: head 
+ *
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
