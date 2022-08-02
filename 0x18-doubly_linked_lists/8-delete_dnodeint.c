e223d0643af8449f999889a6bcf5ac12f131a206
Brandon <brandon_scott585@yahoo.com>
2022-08-02 13:04:27 -0700

    doubly linked

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,3 +10,26 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *temp = NULL;
+	unsigned int i;
+
+	if (*head == NULL || head == NULL)
+	{
+		return (-1);
+	}
+	temp = (*head);
+	if (temp == NULL)
+		return (-1);
+	for (i = 0; index != i; i++)
+	{
+		temp = temp->next;
+	}
+	if (temp->next != NULL)
+	{
+		temp->next->prev = temp->prev;
+	}
+	if (index == 0)
+		(*head) = temp->next = temp->next;
+	free(temp);
+	return (1);
+}
