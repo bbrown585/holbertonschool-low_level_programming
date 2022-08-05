79166c4ad612d2b6f8d689b57f71e1b38fbfdeb2
Brandon <brandon_scott585@yahoo.com>
2022-08-04 18:40:07 -0700

    updating 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash tabel
+ * @key:
+ * @value:
+ * Return: 1 if succeeds, 0 otherwise
+ */
+
+
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: value with a key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *hash;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || (strcmp(key, "") == 0))
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	hash = ht->array[index];
+	
+	hash = ht->array[index];
+	while (hash != NULL)
+	{
+		if (strcmp(hash->key, key) == 0)
+		{	
+			return (hash->value);
+		}
+	hash = hash->next;
+	}
+	
+	return (NULL);
+}
