bac537a80e27ab4d45f407b20ba76bc591d916b7
Brandon <brandon_scott585@yahoo.com>
2022-08-04 19:45:42 -0700

    working on 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,8 +6,29 @@
  * hash_table_print - print a hash table
  * @ht: hash table
  *
- *
+ * Return: 0
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i;
+	unsigned int flag = 0;
+	hash_node_t *location;
 
+	if (!ht)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		location = ht->array[i];
+		while (location)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", location->key, location->value);
+			flag = 1;
+			location = location->next;
+		}
+	}
+}
